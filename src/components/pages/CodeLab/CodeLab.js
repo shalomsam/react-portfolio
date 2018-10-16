de74916a0e92d0baf7a7c8131a44d34fe466e8de
Shalom Sam <shalom.s@endurance.com>
2018-10-17 01:28:34 +0530

    preview link added for react-canvas-js in codelabs section

--- src/components/pages/CodeLab/CodeLab.js
+++ src/components/pages/CodeLab/CodeLab.js
@@ -17,6 +17,7 @@ const CodeLab = () => {
                             <p className="card-text">Awesome HTML canvas animation with React. Currently supports for a beautiful customizable particle animation. Moving towards a holistic canvas animation support.</p>
                             <a href="https://github.com/shalomsam/react-canvas-js" className="card-link">GitHub</a>
                             <a href="https://www.npmjs.com/package/react-canvas-js" className="card-link">NPM</a>
+                            <a href="https://shalomsam.github.io/react-canvas-js/" className="card-link">Preview</a>
                         </div>
                     </div>
                 </div>
